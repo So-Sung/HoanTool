0eecf86450bcf4f4bac513b5e3f2982c71531fab
Jihwan_Kim <77475809+So-Sung@users.noreply.github.com>
2025-02-05 03:51:34 +0900

    Create test.ps1

---
+++ test.ps1
@@ -0,0 +1,19 @@
+$code = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class WinAPI {
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(uint dwDesiredAccess, bool bInheritHandle, uint dwProcessId);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+    [DllImport("kernel32.dll")]
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out uint lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out uint lpThreadId);
+}
+"@
+Add-Type -TypeDefinition $code -Language CSharp
